ad6abe1067a5b8d415b2ea376dac17e591b66f48
Ilushling <bladeg97@mail.ru>
2019-08-11 21:51:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+DATABASE_CONFIG = {
+    'host': '127.0.0.1',
+    'db': 'db',
+    'user': 'root',
+    'password': '',
+    'charset': 'utf8mb4'
+}
+API = {
+    'OWMapikey': '1',
+    'Telegramapikey': '2'
+}
+PROXY = 'socks5h://name@name.name:9999'
